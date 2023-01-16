4a09106f8ed4433d70183b227d4386746743ace3
Zigrino <yulian.zigrino@gmail.com>
2023-01-16 10:45:38 -0500

    Initial Commit

---
+++ VolodyaHasBadIdeas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VolodyaHasBadIdeas
+//
+//  Created by Yulian Itskov-Curto on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
