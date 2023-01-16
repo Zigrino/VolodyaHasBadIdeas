37ee33146aa13cde0a7b2306f0c0761112fb84e0
Zigrino <yulian.zigrino@gmail.com>
2023-01-16 11:00:34 -0500

    initial commit

--- VolodyaHasBadIdeas/ViewController.swift
+++ VolodyaHasBadIdeas/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var pullDown: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPullDownButton()
         // Do any additional setup after loading the view.
+    }
+    func editLabel(text: String){
+        myLabel.text = text
+    }
+    func setPullDownButton(){
+        let optionClosure = {(action : UIAction) in
+            self.editLabel(text: action.title)
+        }
+
+        pullDown.menu = UIMenu(children: [UIAction(title: "Option 1", state: .on, handler: optionClosure), UIAction(title: "Option 2", handler: optionClosure)])
+        pullDown.showsMenuAsPrimaryAction = true
+        pullDown.changesSelectionAsPrimaryAction = true
     }
 
 
